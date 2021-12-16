637c1608958de037e274562f61ffcda131270cd0
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2021-12-16 09:59:51 +0100

    comment  changes

--- demo/src/main/java/com/example/demo/Controller1.java
+++ demo/src/main/java/com/example/demo/Controller1.java
@@ -4,20 +4,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController()
+@RestController() // con esta etiqueta ya se entiende que esta clase es un controlador con sus paths
 public class Controller1 {
 
     @GetMapping("/user/{name}")
-    public String getHola(@PathVariable String name)
+    public String getHola(@PathVariable String name) // pathvariable  = que se mete en el path
     {
         return "Hola " + name;
 
     }
-    List<Person> listPerson = new ArrayList<Person>();
 
     @PostMapping("/useradd")
-    @ResponseBody
+    @ResponseBody // no es necesario usar.
     public Person add(@RequestBody Person person){
+        List<Person> listPerson = new ArrayList<Person>();
         listPerson.add(new Person(person.getName(),person.getAge(),person.getCity()));
         person.setAge(person.getAge()+1);
         return person;
